b96e9c895157d6ad321cf9aa0e42dd3fc1f0083e
Timmmyle <timmyle2010@gmail.com>
2026-07-20 10:19:27 +0700

    style: idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/providers/AuthProvider";
 import { AlertProvider } from "@/components/providers/AlertProvider";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,19 @@ export default function RootLayout({
   return (
     <html lang="vi" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="font-sans bg-[#141412] text-[#F3E5AB] min-h-full flex flex-col selection:bg-[#D4AF37] selection:text-black">
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VPYBVCPZE1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VPYBVCPZE1');
+          `}
+        </Script>
         <AlertProvider>
           <AuthProvider>{children}</AuthProvider>
         </AlertProvider>
